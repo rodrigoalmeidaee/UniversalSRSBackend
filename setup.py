8862f87320f168d8d203c0524011048665a137d9
Rodrigo Almeida <rsalmeidafl@gmail.com>
2017-04-26 12:23:07 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="universalsrs",
+    version="0.1",
+    packages=["universalsrs"],
+    setup_requires=["setuptools_git==1.0b1"],
+    install_requires=[
+        "pymongo",
+        "flask",
+    ],
+    entry_points={
+        "paste.app_factory": "gevent = universalsrs.uwsgi:main",
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ universalsrs/uwsgi.py
@@ -0,0 +1,3 @@
+def main(global_config, **settings):
+    from universalsrs import app
+    return app.app
